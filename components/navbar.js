9b632f9d407180cdea16c9f200b164ddc2e082e5
chandan8809 <chandankrbit3@gmail.com>
2022-01-09 13:42:04 +0530

    Initial commit

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+function navbar(){
+    return ` <div class="navbar">
+    <div><a href="/">Show Random recipe</a></div>
+    <div><a href="/recipeday.html">Recipe of the day</a></div>
+    <div><a href="/random.html">Search recipe</a></div>
+
+</div>`
+}
+export default navbar
